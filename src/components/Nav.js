3173a469aa1a368a463da95377d093909d8cd485
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-07-26 17:20:49 -0400

    Updated Nav component links to include /React-Portfolio

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <ul className="nav nav-tabs">
-      <Link to={"/"} className="nav-item">
+      <Link to={"/React-Portfolio"} className="nav-item">
         About Me
       </Link>
-      <Link to={"/projects"} className="nav-item">
+      <Link to={"/React-Portfolio/projects"} className="nav-item">
         Projects
       </Link>
-      <Link to={"/contact"} className="nav-item">
+      <Link to={"/React-Portfolio/contact"} className="nav-item">
         Contact
       </Link>
-      <Link to={"/resume"} className="nav-item">
+      <Link to={"/React-Portfolio/resume"} className="nav-item">
         Resume
       </Link>
     </ul>
