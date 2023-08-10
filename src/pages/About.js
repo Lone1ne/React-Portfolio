2a0517c33c357c9b136bfa60342b6b8fbf562a33
Lone1ne <ds@Dereks-MacBook-Air.local>
2023-08-10 17:28:11 -0600

    Updated AboutMe section

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,11 +10,14 @@ function About() {
         <div className="text-content">
           <h3>Hi I'm Derek!</h3>
           <p>
-            A highly motivated and detail-oriented full-stack JavaScript
-            developer with a passion for creating web applications. I am
-            currently enrolled in a bootcamp through the University of Denver to
-            further my development skills and I could not be more excited to
-            continue my coding journey!
+            A highly motivated Full-Stack Developer with a passion for
+            developing web applications. Proficient in both front-end and
+            back-end development and backed by a diverse skill set including
+            psychology, business, finance, and customer service. After
+            successfully completing an accelerated programming course and
+            numerous personal and collaborative projects, I consistently
+            demonstrate a strong commitment to learning and developing industry
+            skills.
           </p>
         </div>
       </div>
